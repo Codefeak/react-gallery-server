066c6e9737221251675e4dd7110ac5ff2fc0f036
codefeak <codefever17@gmail.com>
2018-08-01 15:39:44 +0300

    added cors

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require( 'express' );
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require( 'mongoose' );
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-mongoose.connect (process.env.MONGODB_URI || "mongodb+srv://ProblemChild:bzz4uu8eDU@ig001-nt7pi.mongodb.net/UserList", {useNewUrlParser:true})
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://ProblemChild:bzz4uu8eDU@ig001-nt7pi.mongodb.net/UserList", {useNewUrlParser:true})
+
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.set('client', express.static('https://react-nodeexpress-gallery.herokuapp.com:3000'));
-
-const schema1 = {
+app.use(cors());
+const schema = {
     username:{
         type:String,
     },
@@ -17,23 +18,35 @@ const schema1 = {
     }
 };
 
-const User = mongoose.model('User', schema1);
+const schema1 = {
+    firstName:{
+        type:String,
+    },
+    password:{
+        type:String,
+    }
+}
+
+const User = mongoose.model('User', schema);
 
 
-app.get('/', (req,res)=>{  
-
-});
+// app.get('/', (req,res)=>{  
+//     res.redirect('https://react-nodeexpress-gallery.herokuapp.com:3000/');
+// });
 
 app.post('/login', (req,res)=>{
+    console.log('a');
     User.find({username:req.body.username}, (err,user)=>{
         if(err)console.log(err);
-        user.map(item =>{
+        user.map(item=>{
+            console.log(item);
             if(item.password === req.body.password){
-                return(res.json(item));
+                console.log(mongoose);
+                return (res.json(item));
             }
         })
-    })
-})
+    });
+});
 
 
 

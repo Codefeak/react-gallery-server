7d594bb42cde6af85af85ab3d545c7dfccd58947
codefeak <codefever17@gmail.com>
2018-08-03 11:38:44 +0300

    insertone to addNew

--- server.js
+++ server.js
@@ -82,13 +82,13 @@ app.post('/login', (req,res)=>{
 
 app.post('/login/addNew', (req,res)=>{
     console.log('inserted',req.body);
-    // Information.insertMany(req.body, (err,user)=>{
-    //     if(err)console.log('err', err);
-    //     console.log('inserted',req.body);
-    // });
-    // Informations.find({}, (err,users)=>{
-    //     console.log(users);
-    // })
+    Information.insertOne(req.body, (err,user)=>{
+        if(err)console.log('err', err);
+        console.log('inserted',req.body);
+    });
+    Informations.find({}, (err,users)=>{
+        console.log(users);
+    })
 })
 
 

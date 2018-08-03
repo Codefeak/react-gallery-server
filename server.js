5343b10acc1e321a766b8eeae19aef585bd8a29f
codefeak <codefever17@gmail.com>
2018-08-03 17:03:48 +0300

    added delete function console

--- server.js
+++ server.js
@@ -93,10 +93,10 @@ app.post('/login/addNew', (req,res)=>{
 app.delete('/login/delete', (req,res)=>{
     console.log('body', req.body);
     
-    Informations.findOneAndRemove({_id:req.body._id}, (err, user)=>{
+    Informations.findOneAndRemove({ _id:req.body._id }, (err, user)=>{
         if(err)console.log('err', err);
         })
-    Informations.find({}, (err,users)=>{
+    Informations.find({ _id:req.body._id }, (err,users)=>{
         return(res.json(users));
         })
     })

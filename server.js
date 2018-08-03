e4dca5da74409da0b18c5c894e5af1e7077a078b
codefeak <codefever17@gmail.com>
2018-08-03 22:38:38 +0300

    add post according to id

--- server.js
+++ server.js
@@ -86,8 +86,9 @@ app.post('/login', (req,res)=>{
 
 app.post('/login/profile/:id',(req,res)=>{
     console.log('req', req.body)
-    Informations.find({_id:req.body}, (err,user)=>{
+    Informations.findOne({_id:req.body}, (err,user)=>{
         if(err)console.log(err);
+        console.log(user)
         return (res.json(user));
     })
 })

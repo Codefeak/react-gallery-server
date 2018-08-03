59caecbd687d07c70cf63522f24b0865e26b46a8
codefeak <codefever17@gmail.com>
2018-08-03 11:45:23 +0300

    used create instead of insertone to addNew

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post('/login', (req,res)=>{
 
 app.post('/login/addNew', (req,res)=>{
     console.log('inserted',req.body);
-    Informations.insertOne(req.body, (err,user)=>{
+    Informations.create(req.body, (err,user)=>{
         if(err)console.log('err', err);
         console.log('inserted',req.body);
     });

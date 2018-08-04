d4ef3082388760997f4146533becdf19f0ccaca8
codefeak <codefever17@gmail.com>
2018-08-04 15:38:12 +0300

    new branch initial commit

--- server.js
+++ server.js
@@ -1,60 +1,61 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express();
-const mongoose = require( 'mongoose' );
+const mongoose = require('mongoose');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://ProblemChild:bzz4uu8eDU@ig001-nt7pi.mongodb.net/UserList", {useNewUrlParser:true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://ProblemChild:bzz4uu8eDU@ig001-nt7pi.mongodb.net/UserList", { useNewUrlParser: true });
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+
 const schema = {
-    username:{
-        type:String,
+    username: {
+        type: String,
     },
-    password:{
-        type:String,
+    password: {
+        type: String,
     }
 };
 
 const schema1 = {
-    firstName:{
-        type:String,
+    firstName: {
+        type: String,
     },
-    lastName:{
-        type:String,
+    lastName: {
+        type: String,
     },
-    title:{
-        type:String,
+    title: {
+        type: String,
     },
-    nationality:{
-        type:String,
+    nationality: {
+        type: String,
     },
-    src:{
-        type:String,
+    src: {
+        type: String,
     },
-    alt:{
-        type:String,
+    alt: {
+        type: String,
     },
-    skills:{
-        type:Array,
+    skills: {
+        type: Array,
     },
-    whySofterDeveloper:{
-        type:String,
+    whySofterDeveloper: {
+        type: String,
     },
-    longTermVision:{
-        type:String,
+    longTermVision: {
+        type: String,
     },
-    motivatesMe:{
-        type:String,
+    motivatesMe: {
+        type: String,
     },
-    favoriteQuote:{
-        type:String,
+    favoriteQuote: {
+        type: String,
     },
-    joinedOn:{
-        type:String,
+    joinedOn: {
+        type: String,
     }
 }
 
@@ -62,49 +63,60 @@ const User = mongoose.model('User', schema);
 const Informations = mongoose.model('Informations', schema1);
 
 
-app.get('/api', (req,res)=>{ 
-    Informations.find({},(err,info)=>{
-        if(err)console.log(err);
+app.get('/api', (req, res) => {
+    Informations.find({}, (err, info) => {
+        if (err) console.log(err);
         return (res.json(info));
     })
-}); 
+});
 
-app.post('/login', (req,res)=>{
-    User.find({username:req.body.username}, (err,user)=>{
-        if(err)console.log(err);
-        user.map(item=>{
-            if(item.password === req.body.password){
+app.post('/login', (req, res) => {
+    User.find({ username: req.body.username }, (err, user) => {
+        if (err) console.log(err);
+        user.map(item => {
+            if (item.password === req.body.password) {
                 console.log(mongoose);
                 return (res.json(item));
             }
         })
     });
+
 });
 
-app.post('/login/addNew', (req,res)=>{
-    Informations.create(req.body, (err,user)=>{
-        if(err)console.log('err', err);
+app.get('/login/images/:id', (req, res) => {
+    const id = req.params.id;
+    res.sendFile(id, { root: ('assets/images') });
+});
+
+app.get('/login/images/thumbnails/:id', (req, res) => {
+    const id = req.params.id;
+    res.sendFile(id, { root: ('assets/images/thumbnails') });
+});
+
+app.post('/login/addNew', (req, res) => {
+    Informations.create(req.body, (err, user) => {
+        if (err) console.log('err', err);
     });
-    Informations.find({}, (err,users)=>{
-        return(res.json(users));
+    Informations.find({}, (err, users) => {
+        return (res.json(users));
     })
 });
 
 
 
-app.delete('/login/delete', (req,res)=>{
-    req.body.map(item=>{
-        Informations.deleteOne({ _id:item._id }, (err, user)=>{
-            if(err)console.log('err', err);
+app.delete('/login/delete', (req, res) => {
+    req.body.map(item => {
+        Informations.deleteOne({ _id: item._id }, (err, user) => {
+            if (err) console.log('err', err);
         })
     })
-    Informations.find({}, (err,users)=>{
-        return(res.json(users));
+    Informations.find({}, (err, users) => {
+        return (res.json(users));
     })
 });
 
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Listening to port ${port}`);
 })

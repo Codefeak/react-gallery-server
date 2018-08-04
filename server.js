9a3ff5a6923cd70a0ab6da65d85d2e60684102ef
codefeak <codefever17@gmail.com>
2018-08-04 10:34:25 +0300

    testing file upload

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require( 'express' );
 const app = express();
 const mongoose = require( 'mongoose' );
 const cors = require('cors');
+const multer = require('multer');
+const upload = multer({dest:'/tmp/uploads/' , filename:(req,file,cb)=>{cb(null,file.fieldname + 'jpg.webp')}});
 const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://ProblemChild:bzz4uu8eDU@ig001-nt7pi.mongodb.net/UserList", {useNewUrlParser:true});
@@ -9,6 +11,12 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://ProblemChild:bzz4uu8e
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors());
+const handleError = (err, res) => {
+    res
+      .status(500)
+      .contentType("text/plain")
+      .end("Oops! Something went wrong!");
+  };
 const schema = {
     username:{
         type:String,
@@ -80,13 +88,39 @@ app.post('/login', (req,res)=>{
     });
 });
 
-app.post('/login/addNew', (req,res)=>{
-    Informations.create(req.body, (err,user)=>{
-        if(err)console.log('err', err);
-    });
-    Informations.find({}, (err,users)=>{
-        return(res.json(users));
-    })
+// app.post('/login/addNew', (req,res)=>{
+//     Informations.create(req.body, (err,user)=>{
+//         if(err)console.log('err', err);
+//     });
+//     upload.single('src'),(req,res,next)=>{
+
+//     }
+//     Informations.find({}, (err,users)=>{
+//         return(res.json(users));
+//     })
+// });
+
+app.post('login/addNew', upload.single('src'),(req,res)=>{
+    console.log(req.body);
+    const tmpPath = req.file.path;
+    const targetPath = path.join(__dirname, './uploads/images.png');
+    if(path.extname(req.file.originalname).toLowerCase()==='.png'){
+        fs.rename(tmpPath, targetPath, err =>{
+            if (err) return handleError(err, res);
+            res
+                .status(200)
+                .contentType('text/plain')
+                .end('File Uploaded!');
+        });
+    }else {
+        fs.unlink(tmpPath, err =>{
+            if(err)return handleError(err, res);
+            res
+                .status(403)
+                .contentType('text/plain')
+                .end('Only .png files are allowed')
+        });
+    }
 })
 
 app.delete('/login/delete', (req,res)=>{
@@ -98,7 +132,7 @@ app.delete('/login/delete', (req,res)=>{
     Informations.find({}, (err,users)=>{
         return(res.json(users));
     })
-})
+});
 
 
 

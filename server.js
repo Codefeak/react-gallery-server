4e767828ab5c8232373437891407fe8e9726b289
codefeak <codefever17@gmail.com>
2018-08-09 10:01:57 +0300

    delete console log

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.delete('/login/delete', (req, res) => {
     })
 });
 
-app.get('/*', (req, res)=>{
+app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'client','build','index.html'));
  console.log(path.join( 'client','build','index.html'));   
 });

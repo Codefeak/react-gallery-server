12d3bd7148d4097535028c8c74f2fa63c48be28f
codefeak <codefever17@gmail.com>
2018-08-09 11:24:56 +0300

    back to confi_production false

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.delete('/login/delete', (req, res) => {
     })
 });
 
-app.use( express.static(path.join(__dirname, 'client','build')));
+app.use(express.static(path.join(__dirname, 'client','build')));
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'client','build','index.html'));
 });
